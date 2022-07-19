29d7b8e03104d884742aac7beb869879800970fb
Santii2312 <79469290+Santii2312@users.noreply.github.com>
2022-07-19 10:19:26 -0500

    Minimos cuadrado
    Método de minimos cuadrados

--- index.m
+++ index.m
@@ -1,7 +1,8 @@
+  clear;
   opc_sel = 0;
-  itemlist = {"- Bisección", "- Falsa posición", "Metodo 3", "- Secante", "Salir"};
+  itemlist = {"- Bisección", "- Falsa posición", "Metodo 3", "- Secante", "- Minimos Cuadrados", "Salir"};
 
-  while (opc_sel != 5)
+  while (opc_sel != 6)
 
       opc_sel = listdlg ("ListString", itemlist,
       "Name", "Seleccione un método",
@@ -75,7 +76,7 @@
               try
                   biseccion(a,b, funcion)
               catch
-                disp("Error al ejecutar el metodo de biseccion")
+                disp("Error al ejecutar el método de biseccion")
                 return %borrar
               end_try_catch
 
@@ -143,7 +144,7 @@
               try
                   falsa_pos(a,b, funcion)
               catch
-                disp("Error al ejecutar el metodo de Falsa posición")
+                disp("Error al ejecutar el método de Falsa posición")
                 return %borrar
               end_try_catch
 
@@ -163,7 +164,7 @@
               % pedir a y b
               while(error != 0)
                 try
-                  respuesta = inputdlg({"punto de incicio 1", "punto de inicio 2"}, "Rango de trabajo", 1,{a,b});
+                  respuesta = inputdlg({"punto de inicio 1", "punto de inicio 2"}, "Rango de trabajo", 1,{a,b});
                   a = (str2double(respuesta(1)))
                   b = (str2double(respuesta(2)))
 
@@ -214,7 +215,113 @@
               try
                   secante(a,b, funcion)
               catch
-                disp("Error al ejecutar el metodo de la Secante")
+                disp("Error al ejecutar el método de la Secante")
+                return %borrar
+              end_try_catch
+
+          % Minimos Cuadrados
+          case 5
+              clc;
+              error = 1; % si es 0 no hay errores
+              msg_error = "";
+
+              % Pedir cantidad de datos
+              while(error != 0)
+                try
+                  v = 0;
+                  respuesta = inputdlg({"Cantidad de datos"},
+                  "Tamaño del vector", 1,{v});
+                  v = (str2double(respuesta(1)));
+
+                  if(isnan(v) || v < 1)
+                    msg_error = "Tamaño del vector no válido";
+                    disp("Tamaño del vector no válido");
+                    error = 1;
+                    questdlg (strcat("ERROR\n",msg_error), "ERROR");
+                    continue
+                  end
+                  error = 0;
+                  msg_error = "";
+
+                  x = [1:v];
+                  y = [1:v];
+                catch
+                  disp("Error tomando datos del vector")
+                  error = 1;
+                  msg_error = "Error en el proceso";
+                end_try_catch
+
+              endwhile
+
+              error = 1;
+              % Pedir datos
+              while(error != 0)
+                try
+
+                  for i=1:(v)
+                     a = 0;
+                     b = 0;
+                     respuesta = inputdlg({"Dato de x", "Dato de Y"},
+                     "Tamaño del vector", 1,{a, b});
+                     a = (str2double(respuesta(1)));
+                     b = (str2double(respuesta(2)));
+
+                     if(isnan(a) || isnan(b))
+                       msg_error = "Valor de x ó y no válido";
+                       disp("Valor de x ó y no válido");
+                       error = 1;
+                       questdlg (strcat("ERROR\n",msg_error), "ERROR");
+                       continue
+                     end
+
+                     x(i) = a;
+                     y(i) = b;
+
+                  endfor
+
+                  error = 0;
+                  msg_error = "";
+                catch
+                  disp("Error tomando datos a y b")
+                  error = 1;
+                  msg_error = "Error en el proceso";
+                end_try_catch
+
+              endwhile
+
+              x
+              y
+
+              error = 1;
+              % Pedir grado del polinomio
+              while(error != 0)
+                try
+                  n = 3;
+                  respuesta = inputdlg({strcat("Grado del Polinomio ejm:"," 2")}, "Grado", 1,{n});
+                  n = (str2double(respuesta))
+
+                  if(isnan(n))
+                    msg_error = "Valor de n no válido";
+                    disp("Valor de n no válido");
+                    error = 1;
+                    questdlg (strcat("ERROR\n",msg_error), "ERROR");
+                    continue
+                  end
+
+                  error = 0;
+                  msg_error = "";
+                catch
+                  disp("Error pidiendo grado del polinomio")
+                  error = 1;
+                  msg_error = "Error en el proceso";
+                end_try_catch
+              end
+
+
+              try
+                  minimos_cuadrados(x,y,n)
+              catch
+                disp("Error al ejecutar el método de minimos cuadrados")
                 return %borrar
               end_try_catch
           otherwise
